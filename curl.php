0b59c8e92a0c534dd7f289c11b8557ac91df389f
Brian Fitzgerald <brianfit58@gmail.com>
2014-12-22 23:20:56 +0100

    Stripped HTML down to minimum
    Removed HTML crud Pages branding for that lean demo look. Kept my son’s
    stupendously gaudy buttons.

--- curl.php
+++ curl.php
@@ -6,6 +6,8 @@
 //  $service_url = 'http://arduino.local/digital/' . $pin . '/' . $state;
 // The MDNS service kept failing for unknown reasons for me -- I substituted 
 //arduino.local for the IP of the Arduino and all was well. 
+//Substitute the IP address below in TWO places with the IP addressType
+//of your Arduino device 
 $service_url = 'http://192.168.0.15/digital/' . $pin . '/' . $state;
 
   $curl = curl_init($service_url);
@@ -19,7 +21,9 @@ $service_url = 'http://192.168.0.15/digital/' . $pin . '/' . $state;
 
   //Print answer
   echo $curl_response;
-  //Now wait 1 second and turn that pin OFF
+  //Now wait 1 second and turn that pin OFF: you only need a short burst
+  //to fire the infrared signal, and you don't want to leave the 
+  //remote in a perpetual ON state: you'll kill your battery.
   sleep(1); 
   $service_url = 'http://192.168.0.15/digital/' . $pin . '/1';
   $curl = curl_init($service_url);
@@ -29,7 +33,7 @@ $service_url = 'http://192.168.0.15/digital/' . $pin . '/' . $state;
   $curl_response = curl_exec($curl);
   curl_close($curl);
 
-  //Print answer
+  //Print answer. Dance Like nobody's looking.
   echo $curl_response;
 
 ?>
